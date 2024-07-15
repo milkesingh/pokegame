32a677ed3b878cb0001ebf70106df9eb5965970a
Ivan Raab <raabdev@gmail.com>
2024-07-15 20:20:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,30 @@ def get_pokemon_data(pokemon_id):
         return None
 
 
-pokemon_id = "pikachu"
-pokemon_data = get_pokemon_data(pokemon_id)
-if pokemon_data:
-    print(json.dumps(pokemon_data, indent=4))
+# pokemon_id = "pikachu"
+# pokemon_data = get_pokemon_data(pokemon_id)
+# if pokemon_data:
+#     print(json.dumps(pokemon_data, indent=4))
 
 
 def main():
     pokemon_id = input("Ingrese el ID de un Pokémon: ")
     pokemon_data = get_pokemon_data(pokemon_id)
     if pokemon_data:
-        print(json.dumps(pokemon_data, indent=4))
+        # print(json.dumps(pokemon_data, indent=4))
+        nombre_pokemon = pokemon_data['name']
+        print(f"Nombre: {nombre_pokemon}")
+
+        tipos_pokemon = [tipo['type']['name'] for tipo in pokemon_data['types']]
+        print(f"Tipos: {', '.join(tipos_pokemon)}")
+
+        estadisticas_pokemon = {stat['stat']['name']: stat['base_stat'] for stat in pokemon_data['stats']}
+        print("Estadísticas:")
+        for stat_name, stat_value in estadisticas_pokemon.items():
+            print(f"{stat_name}: {stat_value}")
+
+
+
     else:
         print("No se pudo encontrar el Pokémon.")
 
